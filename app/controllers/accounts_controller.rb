1724533ec4728a2424fff28fa8f43b380d07e639
Toby Carter <148788610+Toby-J-Carter@users.noreply.github.com>
2026-03-18 07:57:07 -0400

    Auto invoice creation

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -11,6 +11,42 @@ class AccountsController < ApplicationController
 
   def show
     @account = current_user.account
+    for lease in current_user.leases
+      last_rent_invoice = @account.invoices.where(lease: lease, charge_type: "rent").order(due_date: :desc).first
+      last_due_date = last_rent_invoice&.due_date || lease.start_date.end_of_month
+      amount = @account.automatic_payment_amount(lease.unit.rental_rate)
+      today = Date.current
+
+      while today > last_due_date do
+        invoice = @account.invoices.create(
+          lease: lease,
+          charge_type: "rent",
+          total_charge: amount,
+          due_date: last_due_date >> 1,
+          status: "unpaid"
+        )
+        last_due_date = last_due_date >> 1
+        @account.update_column(:balance, @account.balance + amount)
+      end
+
+      last_utility_invoice = @account.invoices.where(lease: lease, charge_type: "utility").order(due_date: :desc).first
+      last_due_date = last_rent_invoice&.due_date || lease.start_date.end_of_month
+      amount = rand(100)
+      today = Date.current
+
+      ## todo, gotta add utility fields to the lease table
+      while today > last_due_date do
+        invoice = @account.invoices.create(
+          lease: lease,
+          charge_type: "utility",
+          total_charge: amount,
+          due_date: last_due_date >> 1,
+          status: "unpaid"
+        )
+        last_due_date = last_due_date >> 1
+        @account.update_column(:balance, @account.balance + amount)
+      end
+    end
   end
 
   # for payment cycle (show variable rate in front end), and bank transfer info
@@ -29,7 +65,7 @@ class AccountsController < ApplicationController
       redirect_to account_path(current_user.account)
     else
       account = Account.find(params[:id])
-      account.account.set_discount(
+      account.set_discount(
         discount_percentage: params[:discount_percentage]
       )
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   enum :role, { tenant: 0, leasing_agent: 1, administrative_staff: 2 }
 
   has_one :account
+  has_many :leases
   has_many :availabilities, dependent: :destroy
   has_many :appointments, dependent: :destroy
 
