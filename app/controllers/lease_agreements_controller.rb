f9a036f741c49463251cf5ab082d6c94dc97cb58
Toby Carter <tobyc@my.yorku.ca>
2026-03-19 11:42:56 -0400

    autorenew lease

--- app/controllers/lease_agreements_controller.rb
+++ app/controllers/lease_agreements_controller.rb
@@ -7,6 +7,17 @@ class LeaseAgreementsController < ApplicationController
   # specific lease
   def show
     @lease_agreement = Lease.find(params[:id])
+
+    # autorenewal logic
+    if Date.current > @lease_agreement.end_date
+      if @lease_agreement.renewal_policy == "automatic"
+        @lease_agreement.update_column(:end_date, @lease_agreement.end_date >> 12)
+      else
+        @lease_agreement.update_column(:active, false)
+      end
+    else
+      @lease_agreement.update_column(:active, false)
+    end
   end
 
   # for the lease form
