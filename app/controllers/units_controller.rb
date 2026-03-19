25cc24e9401450fb796b2e43bee2f175e35f7ddc
Toby Carter <tobyc@my.yorku.ca>
2026-03-19 12:21:16 -0400

    update

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -23,4 +23,20 @@ class UnitsController < ApplicationController
     unit.save!
   end
 
+  def update
+    @property = Property.find(params[:property_id])
+    @unit = @property.units.find(params[:id])
+    if @unit.update(
+      property_id: params[:property_id],
+      unit_number: params[:unit_number],
+      size: params[:size],
+      rental_rate: params[:rental_rate],
+      classification: params[:rental_rate],
+      status: params[:status],
+      intended_business_purpose: params[:intended_business_purpose],
+    )
+            
+    redirect_to unit_path(@unit), notice: "Unit updated"
+    end
+  end
 end
