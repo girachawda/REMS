62342d250029005235ed8224ef84b96c1efec74a
Toby Carter <tobyc@my.yorku.ca>
2026-03-19 14:30:20 -0400

    1

--- test/controllers/units_controller_test.rb
+++ test/controllers/units_controller_test.rb
@@ -7,4 +7,24 @@ class UnitsControllerTest < ActionDispatch::IntegrationTest
     get property_unit_path(property, unit)
     assert_response :success
   end
+
+  test "should get new" do
+    get new_property_unit_path(properties(:one))
+    assert_response :success
+  end
+
+  test "should get create" do
+    post property_units_path(properties(:one)), params: {
+      rental_application: {
+        property_id: units(:one).property_id,
+        unit_number: units(:one).unit_number,
+        size: units(:one).size,
+        rental_rate: units(:one).rental_rate,
+        classification: units(:one).classification,
+        status: units(:one).status,
+        intended_business_purpose: units(:one).intended_business_purpose,
+      }
+    }
+    assert_response :success
+  end
 end
